73a7910552f7a07a1eff6a5e7997e822c7035f77
Sergey Berdnikov <berdnikausiarhei@gmail.com>
2024-10-20 15:42:10 +0300

    TMS-C32-HM-7-Task-01
    The task 1 of completed
    it is ok commit

---
+++ src/com/teachmeskills/lessons7/task1/job_position/impl/Worker.java
@@ -0,0 +1,17 @@
+package com.teachmeskills.lessons7.task1.job_position.impl;
+
+import com.teachmeskills.lessons7.task1.job_position.JobService;
+
+public class Worker implements JobService {
+    String namePositionAtJob;
+
+    @Override
+    public void showPositionAtJob() {
+        System.out.printf("Name position at job: %s\n",namePositionAtJob);
+    }
+
+    public Worker() {
+        this.namePositionAtJob = "WORKER";
+    }
+
+}
